987744607cf3d55219f61557f6f252de704c7f2a
damnmad <damnmad@b53aeedb-cb62-44e9-bba4-a92948f050a7>
2010-12-15 12:54:56 +0000

    [1-0-8] Separation de Flex pour FlashBuilder
    git-svn-id: http://95.142.168.194/svn/Uralys/Skooairs/trunk@87 b53aeedb-cb62-44e9-bba4-a92948f050a7

--- flex.src/com/skooairs/components/BoardLine.hx
+++
@@ -1,16 +0,0 @@
-
-
-import com.skooairs.entities.Board;
-
-	//-----------------------------------------------------------------------------------//
-	[Bindable]	public function get board():Board {
-		return _board;
-	}
-
-	public function set board(o:Board):void {
-		_board = o;
-	}
-
-	protected var _board:Board;
-
-	//-----------------------------------------------------------------------------------//
--- src/com/skooairs/constants/Constants.java
+++ src/com/skooairs/constants/Constants.java
@@ -3,9 +3,9 @@ package com.skooairs.constants;
 public class Constants {
 
 	public final static Integer MAX_DAILY_CREDITS = 10;
-	public final static Integer GIFT_CREDITS = 100;
+	public final static Integer GIFT_CREDITS = 100; 
 	
-	public static final int BUY_100_CREDITS = 1;
+	public static final int BUY_100_CREDITS= 1;
 	public static final int BUY_300_CREDITS = 2;
 	public static final int BUY_1000_CREDITS = 3;
 	public static final int BUY_2500_CREDITS = 4;
@@ -13,4 +13,6 @@ public class Constants {
 	
 	public static final int FACEBOOK_USER = 1;
 	public static final int GOOGLE_USER = 2;
+	
+	
 }
